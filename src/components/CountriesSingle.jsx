90e064c4bce1f9570d2a0d85c0050540926feb38
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-02-10 12:20:59 +0200

    fetch weather data

--- src/components/CountriesSingle.jsx
+++ src/components/CountriesSingle.jsx
@@ -1,16 +1,31 @@
 import { useState, useEffect, React } from "react";
 import { Container } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const CountriesSingle = () => {
+  const location = useLocation();
+  const country = location.state.country
   const { single } = useParams();
   const [data, updateData] = useState();
+  const [weather, updateWeather] = useState("");
+  const countryTest = {
+    capital: "Helsinki",
+  };
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${countryTest.capital}&appid=${process.env.REACT_APP_API_KEY}`
+      )
+      .then((res) => {
+        updateWeather(res.data);
+      });
+  }, [countryTest.capital]);
 
-  useEffect(()=>{},[])
-  
+  console.log("Weather:", weather);
   return (
     <Container>
-      <div>Single Country will be here</div>
+      <div>{country.name.common}</div>
     </Container>
   );
 };
