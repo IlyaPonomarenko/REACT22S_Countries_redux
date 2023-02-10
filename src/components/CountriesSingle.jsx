5104e360012934cb0ffbff60dd957cfa2ae0eb03
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-02-10 10:09:43 +0200

    init

---
+++ src/components/CountriesSingle.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect, React } from "react";
+import { Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+
+const CountriesSingle = () => {
+  const { single } = useParams();
+  const [data, updateData] = useState();
+
+  return (
+    <Container>
+      <div>Single Country will be here</div>
+    </Container>
+  );
+};
+
+export default CountriesSingle;
